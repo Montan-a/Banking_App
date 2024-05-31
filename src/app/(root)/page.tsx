de1dc8bb0f376f29ce4f663e5aef6bd3411fa32a
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-05-31 09:52:58 -0500

    add header and balance display

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Montana" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <div className="home-header">
+          <HeaderBox
+            user={loggedIn?.firstName || "Guest"}
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and transactions"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.32}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
